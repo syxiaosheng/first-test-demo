dcac4170d559267d0edc253d4946b68393eca7ee
hainiu <hainiu@example.com>
2023-06-07 17:15:46 +0800

    Revert "第一次上传"
    This reverts commit 448039a3

--- src/main/java/com/hainiu/Demo01.java
+++ src/main/java/com/hainiu/Demo01.java
@@ -5,9 +5,5 @@ public class Demo01 {
 
     public static void main(String[] args) {
         System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
-        System.out.println("你好");
     }
 }
